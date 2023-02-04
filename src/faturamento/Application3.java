d871ba4a81a520dacd6f37a1bf3cea0b0293d9a4
Bruno Salata <brunoslima@gmail.com>
2023-02-04 18:43:16 -0300

    Teste 3 - Faturamento

--- src/faturamento/Application3.java
+++ src/faturamento/Application3.java
@@ -1,10 +1,35 @@
 package faturamento;
 
+import java.io.FileReader;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Application3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		JSONParser jsonParser = new JSONParser();
+		try(FileReader reader = new FileReader("dados_json.json")){
+			
+			Object obj = jsonParser.parse(reader);
+			System.out.println(obj);
+
+			JSONArray lista = (JSONArray) obj;
+			
+			lista.forEach(valorDia -> parserFaturamento((JSONObject) valorDia));
+			
+			
+		} catch (Exception e){
+			
+		}
+		
 	}
-
+	
+	public static void parserFaturamento(JSONObject dado) {
+		dado.get("dia");
+		Integer.parseInt(dado.getString("valor").toString());
+	}
+	
 }
