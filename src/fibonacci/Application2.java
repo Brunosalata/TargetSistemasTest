dbbfff0bdf2c04c7b19998a1898b78ffa5bf7c54
Bruno Salata <brunoslima@gmail.com>
2023-02-03 23:36:22 -0300

    Teste 2 - fibonacci

--- src/fibonacci/Application2.java
+++ src/fibonacci/Application2.java
@@ -3,7 +3,23 @@ package fibonacci;
 public class Application2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int n1 = 0, n2 = 1, n3 = 0;
+		int N = 8;
+
+		while (N > n3) {
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+		}
+
+		if (N == 0) {
+			System.out.println(N + " belongs to Fibonacci sequence");
+		} else if (N == n3) {
+			System.out.println(N + " belongs to Fibonacci sequence");
+		} else {
+			System.out.println(N + " doesn't belong to Fibonacci sequence");
+		}
 
 	}
 
